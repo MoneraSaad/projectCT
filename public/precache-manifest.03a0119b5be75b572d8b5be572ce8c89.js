e48cb97387f49920d92ac16e540a6e39605d9b49
Monera Saad <45149167+MoneraSaad@users.noreply.github.com>
2021-01-13 21:36:14 +0200

    public server

---
+++ public/precache-manifest.03a0119b5be75b572d8b5be572ce8c89.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec880ed8611879955b34d84f12c7768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac469dbfd59b4087992",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "2e7577abb9066d9eb61a",
+    "url": "/static/css/main.46656d5c.chunk.css"
+  },
+  {
+    "revision": "9ac469dbfd59b4087992",
+    "url": "/static/js/2.95466ca2.chunk.js"
+  },
+  {
+    "revision": "7412f6c918d76d90d425fbbde74ef344",
+    "url": "/static/js/2.95466ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7577abb9066d9eb61a",
+    "url": "/static/js/main.e2fcf837.chunk.js"
+  },
+  {
+    "revision": "3030031bc6efee04f4fc",
+    "url": "/static/js/runtime-main.c9c43c88.js"
+  },
+  {
+    "revision": "4ddabadb5285ea7fb606e339e8a6c7dc",
+    "url": "/static/media/JiraphLogo.4ddabadb.jpg"
+  },
+  {
+    "revision": "0e4aee1fe591c61e9a1dad9fde7d507d",
+    "url": "/static/media/adminB2.0e4aee1f.png"
+  },
+  {
+    "revision": "25fd7ee8b5e1f70340a016801d75cc70",
+    "url": "/static/media/analyticsB.25fd7ee8.png"
+  }
+]);
---
+++ public/precache-manifest.af5c68e72ab4c401df4e1b34791a6b47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c554c9b70e304504bdd71a682318340b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "487616245935309c878a",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "04dcd25278000a752e42",
+    "url": "/static/css/main.b6581677.chunk.css"
+  },
+  {
+    "revision": "487616245935309c878a",
+    "url": "/static/js/2.3ffaf0a3.chunk.js"
+  },
+  {
+    "revision": "2dfeae871ed0df81b1a1830be7553a53",
+    "url": "/static/js/2.3ffaf0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dcd25278000a752e42",
+    "url": "/static/js/main.5865dad8.chunk.js"
+  },
+  {
+    "revision": "3030031bc6efee04f4fc",
+    "url": "/static/js/runtime-main.c9c43c88.js"
+  },
+  {
+    "revision": "4ddabadb5285ea7fb606e339e8a6c7dc",
+    "url": "/static/media/JiraphLogo.4ddabadb.jpg"
+  },
+  {
+    "revision": "0e4aee1fe591c61e9a1dad9fde7d507d",
+    "url": "/static/media/adminB2.0e4aee1f.png"
+  },
+  {
+    "revision": "25fd7ee8b5e1f70340a016801d75cc70",
+    "url": "/static/media/analyticsB.25fd7ee8.png"
+  }
+]);
