a3d447d7c1e7f2076a1fca7121ad9e0c85269923
Monera Saad <45149167+MoneraSaad@users.noreply.github.com>
2021-01-13 22:44:30 +0200

    json

--- index.js
+++ index.js
@@ -40,6 +40,34 @@ app.use("/api/Driver", driverRouter);
 const childRouter = require("./routing/Children");
 app.use("/api/Children", childRouter);
 
+//new
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'https://finalprojectcwd.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
+
 const port = process.env.PORT || 4000; 
 
 app.listen(port, () => {
